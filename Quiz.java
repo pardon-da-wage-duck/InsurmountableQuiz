7259ebc35fdfc317e7c0c710b2982c62b5daa8ee
BT_2N2_22 <sample@bths.edu>
2023-11-01 10:14:35 -0400

    Started working on the actual game

--- Quiz.java
+++ Quiz.java
@@ -1,9 +1,19 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Quiz {
+
+    private Scanner scan = new Scanner(System.in);
     public static int random(int min, int max){
         return (int)(Math.random()*(max-min)) + min;
     }
 
     public void quizScreen(Userprofile profile){
+
+    }
+
+    public void getQuestions(){
 
     }
 }
