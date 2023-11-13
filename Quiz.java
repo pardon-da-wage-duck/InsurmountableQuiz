cd28eb3ed1a2266c412c9b1939aa21c1c7626ab0
pardon-da-wage-duck <kylej53@nycstudents.net>
2023-11-12 22:58:38 -0500

    worked on the quiz

--- Quiz.java
+++ Quiz.java
@@ -1,29 +1,33 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.List;
 import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Quiz {
 
     private Scanner scan = new Scanner(System.in);
-    private String quizQuestions[] = {};
+    private List<List<String>> quizQuestions= new ArrayList<List<String>>();
+
     public static int random(int min, int max){
         return (int)(Math.random()*(max-min)) + min;
     }
 
     public void quizScreen(Userprofile profile){
         getQuestions();
+        displayQuestions();
 
     }
 
     public void getQuestions(){
         File questions = new File("QuizQuestions.csv");
         try {
-
             Scanner sc = new Scanner(questions);
 
             while (sc.hasNextLine()) {
-                quizQuestions[].add(sc.nextLine());
+                List<String> questionSet = new ArrayList<String>();
+                questionSet.add(sc.nextLine());
+                quizQuestions.add(questionSet);
             }
             sc.close();
         }
@@ -31,4 +35,13 @@ public class Quiz {
             e.printStackTrace();
         }
     }
+
+    public void displayQuestions(){
+        for (int i = 0; i < quizQuestions.size(); i++){
+            for (int j = 0; j< quizQuestions.get(i).size(); j++){
+                System.out.println(quizQuestions.get(i).get(j));
+            }
+        }
+    }
+
 }
